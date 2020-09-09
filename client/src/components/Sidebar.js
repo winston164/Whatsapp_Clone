864a521d38ddf75823a97ebbe1df21ecb92fb8d6
Winston <winstonmartinbendana@gmail.com>
2020-09-09 22:00:01 +0800

    added buttons and lists

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
-import { Tab, Nav } from "react-bootstrap";
+import { Tab, Nav, Button, Modal } from "react-bootstrap";
+import Conversations from './Conversations';
+import Contacts from './Contacts';
+import NewConversationModal from './NewConversationModal';
+import NewContactModal from './NewContactModal';
 
 const CONVERSATION_KEY = "conversations"
 const CONTACTS_KEY = 'contacts'
 
-export default function Sidebar() {
+export default function Sidebar({id}) {
     const [activeKey, setActiveKey] = useState(CONVERSATION_KEY)
+    const[modalOpen, setModalOpen] = useState(false)
+    const conversationsOpen = activeKey === CONVERSATION_KEY
+
+    function closeModal() {
+        setModalOpen(false)
+    }
+
+
     return (
         <div style={{width: '250px'}} className="d-flex flex-column">
             <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
@@ -17,8 +29,27 @@ export default function Sidebar() {
                         <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
                     </Nav.Item>
                 </Nav>
-                <Tab.Content></Tab.Content>
+                <Tab.Content className="border-right overflow-auto flex-grow-1">
+                    <Tab.Pane eventKey={CONVERSATION_KEY} >
+                        <Conversations />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey={CONTACTS_KEY} >
+                        <Contacts />
+                    </Tab.Pane>
+                </Tab.Content>
+                <div className="p-2 border-top border-right small">
+                    Your Id: <span className="text-muted">{id}</span>
+                </div>
+                <Button onClick={() => setModalOpen(true)} className="rounded-0">
+                    New {conversationsOpen ? 'Conversation' : 'Contact'}
+                </Button>
             </Tab.Container>
+
+            <Modal show={modalOpen} onHide={closeModal}>
+                {conversationsOpen ? 
+                <NewConversationModal  closeModal={closeModal}/>:
+                <NewContactModal closeModal={closeModal}/>}
+            </Modal>
         </div>
     )
 }
