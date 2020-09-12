fbe36ef1b7dc6c4bd00c876abb77bfb387afa3f5
Winston <winstonmartinbendana@gmail.com>
2020-09-13 00:23:22 +0800

    Added Conversations Provider to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,21 +3,19 @@ import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Dashboard from "./Dashboard";
 import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/ConversationsProvider";
 
 function App() {
-  const [id, setId] = useLocalStorage('id')
+  const [id, setId] = useLocalStorage("id");
 
   const dashboard = (
     <ContactsProvider>
-      <Dashboard id={id} />
+      <ConversationsProvider id={id}>
+        <Dashboard id={id} />
+      </ConversationsProvider>
     </ContactsProvider>
-  )
-  return (
-    <>
-      {id ? dashboard: <Login onIdSubmit={setId} />}
-      
-    </>
   );
+  return <>{id ? dashboard : <Login onIdSubmit={setId} />}</>;
 }
 
 export default App;
